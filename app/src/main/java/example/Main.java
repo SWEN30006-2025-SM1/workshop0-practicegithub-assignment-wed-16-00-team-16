d7a94024ebf745bf0d9bdde52e90c6a7b79e0d5a
Calebsch <sch1297985168@gmail.com>
2025-03-05 17:22:54 +1100

    6 to 9 for divide

--- app/src/main/java/example/Main.java
+++ app/src/main/java/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("add = " + calculator.add(3, 5));
         System.out.println("minus = " + calculator.minus(8, 2));
         System.out.println("multiply = " + calculator.multiply(3, 5));
-        System.out.println("divide = " + calculator.divide(6, 3));
+        System.out.println("divide = " + calculator.divide(9, 3));
 
     }
 }
